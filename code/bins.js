1ae9b0eafb12e89148ced293b85f1c6e5bdb4ef2
bomsy <b4bomsy@gmail.com>
2015-12-03 16:48:19 -0500

    find all the code sections on the page

--- code/bins.js
+++ code/bins.js
@@ -1,3 +1,6 @@
 (function(window, document) {
-  window.alert("runhajshdfkjahsdkfjas");
+  var elms = document.getElementsByTagName("pre");
+  for (var i = 0; i < elms.length; i++) {
+    elms[i].style.border = "1px red solid";
+  }
 })(window, document);
