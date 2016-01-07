557a3d2018dca844af32be15f7e85aaa026281c9
bomsy <b4bomsy@gmail.com>
2016-01-07 17:45:07 +0000

    load codebin in iframe

--- code/bins.js
+++ code/bins.js
@@ -3,16 +3,38 @@
   var codeBlocks = document.getElementsByTagName("pre");
   var codeBlock = null;
   var button = null;
+  var cb = null;
 
   function findAndLoadCodeButtons() {
     for (var i = 0; i < codeBlocks.length; i++) {
       codeBlock = codeBlocks[i];
       button = document.createElement('i');
       button.id = 'block-button-' + i;
+      button.addEventListener('click', function() {
+        
+      });
       button.className = 'bins-buttons glyphicon glyphicon-list-alt'
       codeBlock.style.border = '1px red solid';
       codeBlock.parentNode.insertBefore( button, codeBlock)
     }
+  }
+
+  function loadCodeBin() {
+    cb = document.createElement('iframe');
+    cb.className = 'bins-iframe';
+    cb.setAttribute('sandbox', 'allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts');
+    cb.setAttribute('frameBorder', '1');
+    cb.setAttribute('name', 'Code Bin Output ');
+    cb.id = 'code';
+    cb.src = 'https://blog.bomsy.org/code/codebin/'
+  }
+
+  function showCodeBin() {
+    cb.style.display = 'block';
+  }
+
+  function hideCodeBin() {
+    cb.style.display = 'none';
   }
 
   // Inject bin css
