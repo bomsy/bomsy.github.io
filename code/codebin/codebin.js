2401f9227da7e384e1872ae090bb398911eeb1d2
bomsy <b4bomsy@gmail.com>
2016-01-08 12:17:51 +0000

    fix css typo

--- code/codebin/codebin.js
+++ code/codebin/codebin.js
@@ -1,10 +1,29 @@
-(function(win, doc) {
-  var CodeMirror = win.CodeMirror;
-  var textArea = doc.getElementById('code');
-  
+(function(window, document) {
+  var CodeMirror = window.CodeMirror;
+  var textArea = document.getElementById('code');
+  var host = 'https://blog.bomsy.org'
+
+  function messageListener(event) {
+    if (event.origin !== host) {
+      return;
+    }
+    setEditorValue(event.data);
+  }  
+
+  if (window.addEventListener) {
+    window.addEventListener('message', messageListener, false);
+  } else {
+    window.attachEvent('onmessage', messageListener);
+  }
+
   var editor = CodeMirror.fromTextArea(textArea, {
     lineNumbers: true,
     mode: 'htmlmixed'
   });
+
+  function setEditorValue(value) {
+    editor.setValue(value);
+  }
+
 
 })(window, document);
